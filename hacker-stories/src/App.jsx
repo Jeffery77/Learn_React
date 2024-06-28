2709bc04e9f58e3ce75105be976765a78d3a7c2a
Jeffery77 <johnmamo129@gmail.com>
2024-06-28 18:23:58 +0000

    React JSX

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-const title = 'React';
+
+function getTitle(title)
+{
+  return title;
+}
 function App() {
  
 
   return (
     <>
       <div>
-      <h1>Hello React</h1>
+      <h1>
+        Hello {getTitle('React')}
+      </h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
       </div>
     </>
   )
