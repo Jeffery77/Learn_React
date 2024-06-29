b6b2ed50fa356cadf1b96e53f88d0c0a0c8a8203
Jeffery77 <johnmamo129@gmail.com>
2024-06-29 14:27:25 +0000

    meet another react component

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -21,20 +21,41 @@ const list = [
 }
 ];
 function App() {
- 
+  return(
+    <div>
+  <h1>
+        My Hacker Stories
+      </h1>
+
+      <Search/>
+
+      <hr />
+
+      <List/>
+      </div>
+  );
+
+}
+function Search(){
+
+  return(
+
+    <div>
+      
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+    </div>
+  );
+}
+
+
+function List(){
 
   return (
     <>
       <div>
-      <h1>
-        My Hacker Stories
-      </h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
      
-
-      <hr />
       <ul>
         {list.map(function (item)
         {
